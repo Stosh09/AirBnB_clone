139f239b8ce6a639dbecd67a2ce79a0cf6d9fef4
Stosh09 <stoshochieng@gmail.com>
2023-10-14 17:02:25 +0300

    Created unittests for the Basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,66 +1,51 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-import os
-from uuid import uuid4, UUID
-from sqlalchemy import Column, Integer, String, Float, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
+""" Module for Base Model """
 
-STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
-if STORAGE_TYPE == 'db':
-    Base = declarative_base()
-else:
-    class Base:
-        pass
-
+import uuid
+import datetime
 class BaseModel:
-
     """
-        attributes and functions for BaseModel class
+    Defines a base class with common attributes and methods for other classes.
+
+    Attributes:
+        id (str): A unique identifier for each object.
+        created_at (datetime): The date and time when the object was created.
+        updated_at (datetime): The date and time when the object was last updated.
     """
-    id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False,
-                        default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False,
-                        default=datetime.utcnow())
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self) -> None:
         """
-            instantiation of new BaseModel Class
+        Initializes the object by setting the id, created_at, and updated_at fields.
         """
-        if kwargs:
-            self.__set_attributes(kwargs)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
-
-    def __set_attributes(self, attr_dict):
-        """
-            private: converts attr_dict values to python class attributes
-        """
-        if 'id' not in attr_dict:
-            attr_dict['id'] = str(uuid4())
-        if 'created_at' not in attr_dict:
-            attr_dict['created_at'] = datetime.utcnow()
-        elif not isinstance(attr_dict['created_at'], datetime):
-            attr_dict['created_at'] = datetime.strptime(
-                attr_dict['created_at'], "%Y-%m-%d %H:%M:%S.%f"
-            )
-        if 'updated_at' not in attr_dict:
-            attr_dict['updated_at'] = datetime.utcnow()
-        elif not isinstance(attr_dict['updated_at'], datetime):
-            attr_dict['updated_at'] = datetime.strptime(
-                attr_dict['updated_at'], "%Y-%m-%d %H:%M:%S.%f"
-            )
-        if STORAGE_TYPE != 'db':
-            attr_dict.pop('__class__', None)
-        for attr, val in attr_dict.items():
-            setattr(self, attr, val)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """
-            returns string type representation of object instance
+        Returns a string representation of the object.
+
+        Returns:
+            str: A string representation of the object.
         """
-        class_name = type(self).__name__
-        return '[{}] ({}) {}'.format(class_name, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Updates the updated_at field with the current date and time.
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+        Converts the object to a dictionary representation.
+
+        Returns:
+            dict: A dictionary representation of the object.
+        """
+        instance_dict = self.__dict__.copy()
+        instance_dict["__class__"] = self.__class__.__name__
+        instance_dict["created_at"] = self.created_at.isoformat()
+        instance_dict["updated_at"] = self.updated_at.isoformat()
+        return instance_dict
